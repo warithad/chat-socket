c9bd5107aa3dde3747115f3624ac26a4c599d9a8
warith <warith.ad02@gmail.com>
2024-01-02 00:50:40 +0100

    correct import module in prisma.test.ts file

--- src/lib/__mocks__/prisma.test.ts
+++ src/lib/__mocks__/prisma.test.ts
@@ -1,7 +1,7 @@
 import { beforeEach } from 'vitest'
 import { mockDeep, mockReset } from 'vitest-mock-extended'
 import type { PrismaClient } from '@prisma/client'
-import { describe } from 'node:test'
+import { describe } from 'vitest'
 
 const prisma = mockDeep<PrismaClient>()
 
